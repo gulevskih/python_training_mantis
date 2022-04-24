c6f9500643d4fc9167a765f183cbd1a4daf6abcc
Stanislav <gulevskih@mail.ru>
2022-04-24 16:02:26 +0300

    рандомное имя для почты

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,6 +1,11 @@
+import string
+import random
 
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def test_signup_new_account(app):
-    username = "user3"
+    username = random_username("user_", 10)
     password = "test"
     app.james.ensure_user_exists(username, password)

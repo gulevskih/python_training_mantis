246510a1aceb5424bcddf635ce51876dab48cddf
gulevskikh <gulevskih@mail.ru>
2022-04-24 17:36:17 +0300

    update test_signup_new_account.py

--- fixture/signup.py
+++ fixture/signup.py
@@ -1,4 +1,4 @@
-
+import re
 
 class SignupHelper:
 
@@ -7,17 +7,18 @@ class SignupHelper:
 
     def new_user(self, username, email, password):
         wd = self.app.wd
-        wd.get(self.app.baseUrl + '/signup_page.php')
+        wd.get(self.app.base_url + '/signup_page.php')
         wd.find_element_by_name("username").send_keys(username)
         wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_css_selector("input[type='submit']").click()
 
-        mail = self.app.mail.get_mail("")
+        mail = self.app.mail.get_mail(username, password, "[MantisBT] Account registration")
         url = self.extract_confirmation_url(mail)
 
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
-        wd.find_element_by_name("password1").send_keys(password)
+        wd.find_element_by_name("password_confirm").send_keys(password)
+        wd.find_element_by_css_selector("input[value='Update User']").click()
 
-    def extract_confirmation_url(self):
-        pass
+    def extract_confirmation_url(self, text):
+        return re.search("http://.*$", text, re.MULTILINE).group(0)

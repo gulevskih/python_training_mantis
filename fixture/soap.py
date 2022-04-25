0d7e200bf147f41d068f64bedddd886ef9e72b49
Stanislav <gulevskih@mail.ru>
2022-04-25 18:57:59 +0300

    test_add_project получил проверку списка проектов через soap

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 from suds.client import Client
 from suds import WebFault
+from model.project import Project
 
 class SoapHelper:
 
@@ -16,5 +17,10 @@ class SoapHelper:
 
     def get_list_projects(self, username, password):
         client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
-        l = client.service.mc_projects_get_user_accessible(username, password)
-        return l
+        project_list = []
+        for project in client.service.mc_projects_get_user_accessible(username, password):
+            id = project.id
+            name = project.name
+            description = project.description
+            project_list = project_list.append(Project(id=id, name=name, description=description))
+        return project_list
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,9 +1,16 @@
 from model.project import Project
 
 def test_add_project(app):
-    app.session.login("administrator", "root")
-    old_projects = app.project.count()
-    app.project.create(Project(name="new2", description="qwerty2"))
-    new_projects = app.project.count()
-    assert old_projects + 1 == new_projects
-    print(app.soap.get_list_projects("administrator", "root"))
+    login = "administrator"
+    password = "root"
+    project = Project(name="new5", description="qwerty5")
+    app.session.login(login, password)
+    #old_projects = app.project.count()
+    old_projects = app.soap.get_list_projects(login, password)
+    app.project.create(Project(project))
+    #new_projects = app.project.count()
+    new_projects = app.soap.get_list_projects(login, password)
+    #assert old_projects + 1 == new_projects
+    new_projects.append(Project(project))
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+    print(app.soap.get_list_projects(login, password))
